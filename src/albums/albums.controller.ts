a1a6989011780fcd1585c3d2d0e8eb1010600547
inord777dev <inord777dev@gmail.com>
2022-07-16 22:31:38 +0300

    feat: add albums

---
+++ src/albums/albums.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  HttpCode,
+  ParseUUIDPipe,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AlbumsService } from './albums.service';
+import { CreateAlbumDto } from './dto/create-album.dto';
+import { UpdateAlbumDto } from './dto/update-album.dto';
+
+@Controller('album')
+export class AlbumsController {
+  constructor(private readonly albumsService: AlbumsService) {}
+
+  @Get()
+  @HttpCode(200)
+  findAll() {
+    return this.albumsService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.albumsService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(201)
+  create(@Body(new ValidationPipe()) createAlbumDto: CreateAlbumDto) {
+    return this.albumsService.create(createAlbumDto);
+  }
+
+  @Put(':id')
+  @HttpCode(200)
+  update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body(new ValidationPipe()) updateAlbumDto: UpdateAlbumDto,
+  ) {
+    return this.albumsService.update(id, updateAlbumDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.albumsService.remove(id);
+  }
+}
