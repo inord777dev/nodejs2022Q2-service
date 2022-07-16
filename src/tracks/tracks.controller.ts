f17e175bd168cdcc97e606979962f6d2423cb356
inord777dev <inord777dev@gmail.com>
2022-07-16 22:34:18 +0300

    feat: add tracks

---
+++ src/tracks/tracks.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  Put,
+  ParseUUIDPipe,
+  ValidationPipe,
+} from '@nestjs/common';
+import { TracksService } from './tracks.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+
+@Controller('track')
+export class TracksController {
+  constructor(private readonly tracksService: TracksService) {}
+
+  @Get()
+  @HttpCode(200)
+  findAll() {
+    return this.tracksService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.tracksService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(201)
+  create(@Body(new ValidationPipe()) createTrackDto: CreateTrackDto) {
+    return this.tracksService.create(createTrackDto);
+  }
+
+  @Put(':id')
+  @HttpCode(200)
+  update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body(new ValidationPipe()) updateTrackDto: UpdateTrackDto,
+  ) {
+    return this.tracksService.update(id, updateTrackDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.tracksService.remove(id);
+  }
+}
