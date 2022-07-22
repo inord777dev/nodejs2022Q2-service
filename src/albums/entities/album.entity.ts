0b8b71e5895126eeaae3321a9b974f95b1c22bfb
inord777dev <inord777dev@gmail.com>
2022-07-22 12:39:27 +0300

    feat: albums test complete

--- src/albums/entities/album.entity.ts
+++ src/albums/entities/album.entity.ts
@@ -4,6 +4,7 @@ import {
   PrimaryGeneratedColumn,
   ManyToOne,
   OneToMany,
+  JoinColumn,
 } from 'typeorm';
 import { Artist } from 'src/artists/entities/artist.entity';
 import { Track } from 'src/tracks/entities/track.entity';
@@ -20,7 +21,11 @@ export class Album {
   year: number;
 
   @ManyToOne(() => Artist, (artist) => artist.albums)
+  @JoinColumn()
   artist: Artist;
+
+  @Column({ nullable: true })
+  artistId: string;
 
   @OneToMany(() => Track, (track) => track.album)
   tracks: Track[];
